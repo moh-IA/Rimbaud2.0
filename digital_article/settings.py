5f311479d1d446f18850b7786dd0c5ff8549bf5c
moh-IA <mohamed.el.arbi.rabah@gmail.com>
2021-06-06 02:59:23 +0200

    add STATIC_ROOT

--- digital_article/settings.py
+++ digital_article/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import psycopg2
 import os
+import mimetypes
+
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
